b54159d09e9f862f198a1dc776ea1f69f875d0ea
aktarulrahul <rahul.aktarul@gmail.com>
2021-04-29 02:53:09 +0600

    add and delete cities from the popup

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,16 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
+import { Box, Grid, InputBase, IconButton, Paper } from '@material-ui/core'
+import { Add as AddIcon } from '@material-ui/icons'
 import 'fontsource-roboto'
 import './popup.css'
 import WeatherCard from './weatherCard'
 
 const App: React.FC<{}> = () => {
+  const [cities, setCities] = useState<string[]>([
+    'Dhaka',
+    'Finland',
+    'Toronto',
+    'Error',
+  ])
+  const [cityInput, setCityInput] = useState<string>('')
+  const handleCityButtonClick = () => {
+    if (cityInput == '') {
+      return
+    }
+    setCities([...cities, cityInput])
+    setCityInput('')
+  }
+  const handleCityDeleteButtonClick = (index: number) => {
+    cities.splice(index, 1)
+    setCities([...cities])
+  }
   return (
-    <div>
-      <WeatherCard city="Dhaka" />
-      <WeatherCard city="Finland" />
-      <WeatherCard city="error" />
-    </div>
+    <Box mx="8px" my="16px">
+      <Grid container>
+        <Grid item>
+          <Paper>
+            <Box px="15px" py="5px">
+              <InputBase
+                placeholder="Add a City name"
+                value={cityInput}
+                onChange={(event) => setCityInput(event.target.value)}
+              />
+              <IconButton onClick={handleCityButtonClick}>
+                <AddIcon />
+              </IconButton>
+            </Box>
+          </Paper>
+        </Grid>
+      </Grid>
+      {cities.map((city, index) => (
+        <WeatherCard
+          city={city}
+          key={index}
+          onDelete={() => handleCityDeleteButtonClick(index)}
+        />
+      ))}
+      <Box height="16px" />
+    </Box>
   )
 }
 
--- src/popup/weatherCard/WeatherCard.tsx
+++ src/popup/weatherCard/WeatherCard.tsx
@@ -1,16 +1,31 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Card, CardContent, Typography } from '@material-ui/core'
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from '@material-ui/core'
 import './WeatherCard.css'
 
 import { fetchOpenWeatherData, OpenWeatherData } from '../../utils/api'
 
-const WeatherCardContainer: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+const WeatherCardContainer: React.FC<{
+  children: React.ReactNode
+  onDelete?: () => void
+}> = ({ children, onDelete }) => {
   return (
     <Box mx={'4xpx'} my={'16px'}>
       <Card>
         <CardContent>{children}</CardContent>
+        <CardActions>
+          {onDelete && (
+            <Button color="secondary" onClick={onDelete}>
+              Delete
+            </Button>
+          )}
+        </CardActions>
       </Card>
     </Box>
   )
@@ -20,7 +35,8 @@ type WeatherCardState = 'loading' | 'error' | 'ready'
 
 const WeatherCard: React.FC<{
   city: string
-}> = ({ city }) => {
+  onDelete?: () => void
+}> = ({ city, onDelete }) => {
   const [weatherData, setWeatherData] = useState<OpenWeatherData | null>(null)
   const [cardState, setCardState] = useState<WeatherCardState>('loading')
   useEffect(() => {
@@ -34,7 +50,7 @@ const WeatherCard: React.FC<{
 
   if (cardState == 'loading' || cardState == 'error') {
     return (
-      <WeatherCardContainer>
+      <WeatherCardContainer onDelete={onDelete}>
         <Typography variant="body1">
           {cardState == 'loading'
             ? 'Loading...'
@@ -45,7 +61,7 @@ const WeatherCard: React.FC<{
   }
 
   return (
-    <WeatherCardContainer>
+    <WeatherCardContainer onDelete={onDelete}>
       <Typography variant="h5">{weatherData.name}</Typography>
       <Typography variant="body1">
         {Math.round(weatherData.main.temp)}
